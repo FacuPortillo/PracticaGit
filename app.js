6e25b0e0ca5dbd8f1242ad2d9f8c6f21cd1c02cd
portillo facundo <facundohportillo1995@gmail.com>
2026-01-23 23:58:09 -0300

    Saludo

--- app.js
+++ app.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-console.log("Cambio realizado desde register");
-=======
-console.log("Cambio realizado desde la rama feature/login ahora");
->>>>>>> 947ef882e0b8d81883bec06a2dab0ba6aeb0160c
+console.log("Holaaaa");
